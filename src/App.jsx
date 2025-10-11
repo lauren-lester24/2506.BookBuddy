f5b64a4565432a0525ee5556dedfa01e35a66c0b
lauren-lester24 <lauren.lester24@yahoo.com>
2025-10-11 13:32:26 -0500

    completed Router and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import {Routes, Route} from 
+import { Routes , Route } from "react-router";
 
 
 import Layout from "./layout/Layout";
-import Navbar from "./layout/Navbar";
-
-
+import Register from "./auth/Register.jsx"
+import Login from "./auth/Login"
+import BookDetails from "./Books/BookDetails"
+import BookList from "./Books/BookList"
+import Error404 from "./auth/Error404.jsx";
 
 export default function App() {
-  return <></>;
+
+
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/books" element={<BookList />} />
+
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,10 +1,33 @@
-import { NavLink } from "react-router";
+import { Link } from "react-router";
 
 import { useAuth } from "../auth/AuthContext"
 
 export default function Navbar() {
-return(
+    const { loggedIn, logout } = useAuth();
+return (
+  <Header>
+    <p>Book Buddy</p>
+    <nav>
+      <Link to="/books">Books</Link>
 
-)
+      {loggedIn ? (
+        <>
+          <Link to="/account">Account</Link>
+          <a onClick={() => logout()}>Logout</a>
+        </>
+      ) : (
+        <>
+          <Link to="/register">Register</Link>
+
+          <Link to="/login">Login</Link>
+        </>
+      )}
+    </nav>
+
+    <button onClick={isLoggedIn ? logout : login}>
+      {isLoggedIn ? "Log out" : "Log in"}
+    </button>
+  </Header>
+);
 
 }
