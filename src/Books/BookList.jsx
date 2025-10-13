9902aa62f161004b07909b077815df4e46b3979d
lauren-lester24 <lauren.lester24@yahoo.com>
2025-10-13 18:31:02 -0500

    creating booklistdetails

--- src/Books/BookList.jsx
+++ src/Books/BookList.jsx
@@ -1,4 +1,33 @@
-export default function BookList() {
+import { useState, useEffect } from "react";
+import
+import { getBooks } from "..api/client"
+
+
+export default function BookList({setBookId}) {
+const [ books, setBooks] = useState([]);
+
+useEffect(() => {
+    const syncBooks = async () => {
+        const data = await getBooks();
+        setBooks(data);
+    }
+    syncBooks();
+}, []);
+
+return (
+    <>
+    <h1>Catalog</h1>
+    <ol>
+{books.map((book) => (
+    <li
+    key={books.id}
+    ></li>
+))}
+
+    </ol>
+    
+    </>
+)
 
     
 }
